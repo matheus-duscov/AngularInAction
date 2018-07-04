6f2e1313be156fc7e2eb77daeaa1481a83793b27
matheus-duscov <39441796+matheus-duscov@users.noreply.github.com>
2018-07-04 10:57:07 -0300

    Add files via upload

---
+++ Invoice/invoice-start/src/app/hours-control/hours-control.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-hours-control',
+  templateUrl: './hours-control.component.html',
+  styleUrls: ['./hours-control.component.css']
+})
+export class HoursControlComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
